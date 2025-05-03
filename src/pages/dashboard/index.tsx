fa0c1f2cc41884268f92dc33ffa5b3b79c598e8b
Raí <devvandenberg>
2025-05-02 21:21:36 -0300

    criando outra section

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.css'
 import Head from 'next/head'
 import { getSession } from 'next-auth/react'
 import { TextArea } from '@/components/textarea'
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
 
 export default function Dashboard() {
     return (
@@ -34,6 +36,44 @@ export default function Dashboard() {
                     </form>
                 </div>
 
+            </section>
+
+            <section className={styles.taskContainer}>
+                <h1>Minhas Tarefas</h1>
+
+                <article className={styles.task}>
+                    <div className={styles.tagContainer}>
+                        <label className={styles.tag} >PUBLICO</label>
+                        <button className={styles.shareButton}>
+                            <FiShare2 size={22} color="#0f0f0f" />
+                        </button>
+                    </div>
+
+                    <div className={styles.taskContent}>
+                        <p>Minha tarefa de exemplo</p>
+                        <button className={styles.trashButton} >
+                            <FaTrash size={24} color="#ff0000" />
+                        </button>
+
+                    </div>
+                </article>
+
+                <article className={styles.task}>
+                    <div className={styles.tagContainer}>
+                        <label className={styles.tag} >PUBLICO</label>
+                        <button className={styles.shareButton}>
+                            <FiShare2 size={22} color="#0f0f0f" />
+                        </button>
+                    </div>
+
+                    <div className={styles.taskContent}>
+                        <p>Estudar javascript</p>
+                        <button className={styles.trashButton} >
+                            <FaTrash size={24} color="#ff0000" />
+                        </button>
+
+                    </div>
+                </article>
             </section>
            </main>
         </div>
